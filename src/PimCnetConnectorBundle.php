bad934b5220cd2d52632141e0bc43ffbe7f63946
Romain Monceau <romain@akeneo.com>
2016-11-24 15:32:24 +0100

    Fix measures + add compiler pass

--- src/PimCnetConnectorBundle.php
+++ src/PimCnetConnectorBundle.php
@@ -11,4 +11,14 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class PimCnetConnectorBundle extends Bundle
 {
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $measuresConfigDir = __DIR__ . '/Resources/config/measures';
+        $container->addCompilerPass(new MeasuresCompilerPass($measuresConfigDir));
+    }
 }
