f4a3d18a5754ecf7bcb3d32e0daa87fbdc003b22
Romain Monceau <romain@akeneo.com>
2017-01-16 15:09:14 +0100

    Remove useless code

--- src/PimCnetConnectorBundle.php
+++ src/PimCnetConnectorBundle.php
@@ -15,14 +15,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class PimCnetConnectorBundle extends Bundle
 {
-    /**
-     * @param ContainerBuilder $container
-     */
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        $measuresConfigDir = __DIR__ . '/Resources/config/measures';
-        $container->addCompilerPass(new MeasuresCompilerPass($measuresConfigDir));
-    }
 }
